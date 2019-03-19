754313a955045865fda6f069d78996276dfdc6bf
Victor Baptista <victor.baptista@mzgroup.com>
2019-03-18 22:40:16 -0300

    Exercicio 2

---
+++ SIN3AN_MCA_PROGMULTI/SideMenuApp_Ex2/appBooks/src/app/entities/livro.ts
@@ -0,0 +1,9 @@
+export class Livro {
+    titulo:string;
+    subtitulo:string;
+    capa:string;
+    editora:string;
+    autor:string;
+    publicacao:string;
+    paginas:string;
+}
---
+++ SIN3AN_MCA_PROGMULTI/SideMenuApp_Ex2/appBooks/src/app/livro.ts
@@ -0,0 +1,12 @@
+// import { Autor } from "./autor";
+// import { Editora } from "./editora";
+
+export interface Livro {
+    titulo:string;
+    subtitulo:string;
+    capa:string;
+    editora:string;
+    autor:string;
+    publicacao:string;
+    paginas:string;
+}
